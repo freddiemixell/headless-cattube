61e82baf95ab9192551b1c09563f3a929f41abcd
Frederick Mixell <frederick.mixell@leftfieldlabs.com>
2024-05-23 15:27:20 -0400

    removing app router syntax

--- frontend/components/LikeButton.tsx
+++ frontend/components/LikeButton.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useState } from "react";
 import { FaHeart } from "react-icons/fa"
 
--- frontend/components/VideoPlayer.tsx
+++ frontend/components/VideoPlayer.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {useEffect, useRef} from 'react';
 import {Video} from '@/@types/video';
 
